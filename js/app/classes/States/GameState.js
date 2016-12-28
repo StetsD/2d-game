3afc8005b2d11caabcd877c3041f677f6b5da5b7
StetsD <Dmitriy.Stets@adwatch.ru>
2016-12-28 18:04:30 +0300

    Player class edit

--- js/app/classes/States/GameState.js
+++ js/app/classes/States/GameState.js
@@ -1,33 +1,20 @@
-define(['State', 'Assets'], function(State, Assets){
-
+define(['State', 'Player'], function(State, Player){
+	var x = 0;
+	var y = 0;
 	var GameState = State.extend({
 		init: function(_handler){
 			this._super(_handler);
+			this.player = new Player(_handler, 20, 20);
+		},
+
+		tick: function(_dt){
+			this.player.tick(_dt);
+		},
+
+		render: function(_g){
+			this.player.render(_g)
 		}
 	});
-
-	var x = 0;
-	var y = 0;
-
-	GameState.prototype.tick = function(_dt){
-		if(this.handler.getKeyManager().up){
-			y -=50*_dt;
-		}
-		if(this.handler.getKeyManager().down){
-			y +=50*_dt;
-		}
-		if(this.handler.getKeyManager().left){
-			x -=50*_dt;
-		}
-		if(this.handler.getKeyManager().right){
-			x +=50*_dt;
-		}
-
-	};
-
-	GameState.prototype.render = function(_g){
-		_g.myDrawImage(Assets.getAsset('mario').idle, x, y, Assets.getAsset('mario').width, Assets.getAsset('mario').height);
-	};
 
 	return GameState;
 
--- js/app/classes/display/Display.js
+++ js/app/classes/display/Display.js
@@ -8,6 +8,19 @@ define(['Jquery', 'Class'], function($, Class){
 			width = _width;
 			height = _height;
 			createDisplay();
+		},
+		//Getters
+		getTitle: function(){
+			return title;
+		},
+		getWidth: function(){
+			return width;
+		},
+		getHeight: function(){
+			return height;
+		},
+		getGraphics: function(){
+			return graphics;
 		}
 	});
 
@@ -18,23 +31,6 @@ define(['Jquery', 'Class'], function($, Class){
 		body.innerHTML = "<canvas id='canvas' width='"+width+"' height='"+height+"'></canvas>";
 		graphics = document.getElementById('canvas').getContext('2d');
 	}
-
-	//Getters
-	Display.prototype.getWidth = function(){
-		return width;
-	};
-
-	Display.prototype.getHeight = function(){
-		return height;
-	};
-
-	Display.prototype.getTitle = function(){
-		return title;
-	};
-
-	Display.prototype.getGraphics = function(){
-		return graphics;
-	};
 
 	CanvasRenderingContext2D.prototype.myDrawImage = function(asset, _x, _y, _width, _height){
 		this.drawImage(asset.sheet, asset.x, asset.y, asset.width, asset.height, _x, _y, _width, _height);
